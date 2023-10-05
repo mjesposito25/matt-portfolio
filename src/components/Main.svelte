1762c1a05163957af97d5c1df9134a2231397dd1
mattespositotech <matt@teamduality.dev>
2023-10-05 17:45:59 -0500

    netlify form test

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -90,52 +90,21 @@
         {/each}
     </div>
     <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">The <span class='text-violet-400 poppins'>Complete</span> Package</h5>
-    <div class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full">
-        <table class="bg-white text-slate-700 rounded text-center">
-            <thead class="border-b border-solid border-slate-200">
-                <tr class="">
-                    <th />
-                    <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
-                    <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
-                    <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
-                    <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8">Me</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="border-b border-solid border-slate-200">
-                    <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Dedication</td>
-                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                </tr>
-                <tr class="border-b border-solid border-slate-200">
-                    <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Critical Thought</td>
-                    <td><i class="fa-solid fa-xmark fa-check text-green-500"></i></td>
-                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                </tr>
-                <tr class="border-b border-solid border-slate-200">
-                    <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Interpersonal Skills</td>
-                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                </tr>
-                <tr class="border-b border-solid border-slate-200">
-                    <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Programming Ablity</td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-                    <td><i class="fa-solid fa-check text-green-500"></i></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
-        <p>Scroll to see more &rarr;</p>
-    </div>
+    <form name="contact" method="POST" netlify>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      
     <p class="mx-auto">So why not invest?</p>
   </section>
 </main>
